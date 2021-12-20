a62f78f99d4e902c33990f672b11e2560caee549
Iqbal-Yasir <iamyasir07@gmail.com>
2021-12-20 17:30:39 +0530

    Increment Button

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button className="btn btn-primary">Increment</button>
+      </React.Fragment>
+    );
+  }
+}
